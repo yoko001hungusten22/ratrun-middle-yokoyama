61415cba3793eb8756d82724ab3efe2592c93cb3
yoko001hungusten22 <hysteric.barbie91@gmail.com>
2023-09-19 18:35:58 +0900

    Create main section

---
+++ components/atoms/Accordions/BasicAccordion.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="toggleAccordion()">{{ title }}</button>
+    <div v-show="isOpen">
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, default: "アコーディオン" },
+    description: { type: String, default: "説明" },
+  },
+  setup() {
+    const isOpen = ref(false);
+    const toggleAccordion = () => {
+      // if (isOpen.value === true) {
+      //   isOpen.value = false;
+      //   console.log("trueです");
+      // } else {
+      //   isOpen.value = true;
+      //   console.log("falseです");
+      // }
+      isOpen.value = !isOpen.value;
+    };
+    return { isOpen, toggleAccordion };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- components/organisms/Header/AppHeader.vue
+++ components/organisms/Header/AppHeader.vue
@@ -29,6 +29,10 @@ header {
   width: 100%;
   height: 84px;
   margin: 0 auto;
+  position: absolute;
+  top: 0;
+  left: 0%;
+  z-index: 1;
 }
 
 header h2 {
